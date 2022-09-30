6332f3fbb3968d73df152ddd98154d1fd7effa55
abelmuro <abelmuro93@gmail.com>
2022-09-29 19:57:09 -0700

    almost done with image carousel

--- src/components/App/ImageCarousel/ImageCarousel.js
+++ src/components/App/ImageCarousel/ImageCarousel.js
@@ -1,33 +1,75 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useRef} from 'react';
 import styles from './styles.module.css';
 import referenceOne from './images/referenceOne.png';
 import referenceTwo from './images/referenceTwo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {} from '@fortawesome/free-solid-svg-icons';
+import {faArrowLeft, faArrowRight} from '@fortawesome/free-solid-svg-icons';
 
 
 function ImageCarousel(){
-    const allSlides = useRef();
+    const tracker = useRef();
 
-    useEffect(() => {
+    function moveTracker(currentSlide, prevOrNextSlide) {
+        let newTrackerPosition;
+        currentSlide.classList.remove(styles.currentSlide)
+        prevOrNextSlide.classList.add(styles.currentSlide);
+        newTrackerPosition = prevOrNextSlide.getAttribute("data-position");  
+        tracker.current.style.left = newTrackerPosition + "px";
+    }
 
+    const changeSlide = (e) => {
+        const leftOrRight = e.target.classList.toString();
+        let currentSlide = document.querySelector("." + styles.currentSlide);
 
-    })
+        if(leftOrRight == styles.arrowBackgroundLeft){
+            let prevSlide = currentSlide.previousSibling;
+            if(prevSlide)
+                moveTracker(currentSlide, prevSlide);
+        }
 
+        else {
+            let nextSlide = currentSlide.nextSibling;
+            if(nextSlide)
+                moveTracker(currentSlide, nextSlide);
+        }
+    }
+
+    const changeSlideBasedOnDot = (e) => {
+        let moveTrackerThisAmount;
+        let clickedDot = e.target.closest("." + styles.dot);
+        moveTrackerThisAmount = clickedDot.getAttribute("data-position");
+        tracker.current.style.left = moveTrackerThisAmount + "px";
+
+        const allSlides = Aray.from(tracker.current.children);
+        allSlides.forEach((slide) => {
+            if(slide.classList.contains("." + styles.currentSlide)){
+                slide.classList.remove("." + styles.currentSlide)
+            }
+
+        })
+
+        
+    }
 
     return(
         <section className={styles.background}>
-            <div className={styles.container}>
+            <h1 className={styles.testimonials}>
+                Testimonials
+            </h1>
+
+            <div className={styles.container}>                
 
                 <div className={styles.window}>
-                    <div className={styles.tracker} ref={allSlides}>
-                        <div className={[styles.slideContainer, styles.currentSlide].join(" ")}>
+                    <div className={styles.tracker} ref={tracker}>
+                        <div data-position={0} className={[styles.slide, styles.currentSlide].join(" ")}>
                             <img src={referenceOne} className={styles.slideImage}/>
                             <p className={styles.desc}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                                sed do eiusmod tempor incididunt ut labore et dolore magna 
-                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                                ullamco laboris nisi ut aliquip ex ea commodo consequat.                                
+                                "Lorem ipsum dolor sit amet, 
+                                consectetur adipiscing elit, 
+                                sed do eiusmod tempor incididunt 
+                                ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud 
+                                exercitation ullamco laboris."                                
                             </p>
                             <h2 className={styles.title}>
                                 Samuel Schick
@@ -36,13 +78,15 @@ function ImageCarousel(){
                                 President, CEO
                             </p>
                         </div>
-                        <div className={styles.slideContainer}>
+                        <div data-position={-500} className={styles.slide}>
                             <img src={referenceTwo} className={styles.slideImage}/>
                             <p className={styles.desc}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                                sed do eiusmod tempor incididunt ut labore et dolore magna 
-                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                                ullamco laboris nisi ut aliquip ex ea commodo consequat.                                
+                                "Lorem ipsum dolor sit amet, 
+                                consectetur adipiscing elit, 
+                                sed do eiusmod tempor incididunt 
+                                ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud 
+                                exercitation ullamco."                            
                             </p>
                             <h2 className={styles.title}>    
                                 Jennie Roberts
@@ -53,12 +97,25 @@ function ImageCarousel(){
                         </div>
                     </div>
                 </div>
-                
+                <div className={styles.arrowBackgroundLeft} onClick={changeSlide}>
+                    <span className={styles.eventBubbling}>
+                       <FontAwesomeIcon icon={faArrowLeft} className={styles.arrowLeft}/> 
+                    </span>
+                </div>
+
+                <div className={styles.arrowBackgroundRight} onClick={changeSlide}>
+                    <span className={styles.eventBubbling}>
+                        <FontAwesomeIcon icon={faArrowRight} className={styles.arrowRight}/> 
+                    </span>                   
+                </div>
+
+                <div className={styles.dotNav} onClick={changeSlideBasedOnDot}>
+                    <div className={styles.dot} data-position={0}></div>
+                    <div className={styles.dot} data-position={-500}></div>
+                </div> 
+            
             </div>
-            <div className={styles.dotNav}>
-                <div className={styles.dot}></div>
-                <div className={styles.dot}></div>
-            </div>
+
         </section>
         )
 
